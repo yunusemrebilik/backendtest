6172a84291a2caf06dd4af5938b57f1fcf94a593
Yunus Emre <yunuspider@gmail.com>
2025-07-11 14:24:59 +0300

    Frontend activated

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
