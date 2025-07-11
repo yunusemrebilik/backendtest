722c2d6c29d4f3dd954b89af1851edf67c4afa85
Yunus Emre <yunuspider@gmail.com>
2025-07-11 14:56:38 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 let notes = [  
@@ -35,13 +34,8 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 app.use(requestLogger)
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>')
-// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
